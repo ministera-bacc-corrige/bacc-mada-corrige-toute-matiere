6bb17a4208fc86182148a4b1755692564c03ebbc
ratomgit <ratomixa@gmail.com>
2021-01-09 22:00:40 +0100

    Updates

---
+++ precache-manifest.47d74fd90b34d0e2c778e3f04f038be5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f25d5418b70394679c68527a97cfbe0",
+    "url": "/bacc-mada-corrige-toute-matiere/index.html"
+  },
+  {
+    "revision": "d946492a76c6bf42b031",
+    "url": "/bacc-mada-corrige-toute-matiere/static/css/main.7bee1c80.chunk.css"
+  },
+  {
+    "revision": "fff06567c165c609f926",
+    "url": "/bacc-mada-corrige-toute-matiere/static/js/2.ee2d21fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bacc-mada-corrige-toute-matiere/static/js/2.ee2d21fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d946492a76c6bf42b031",
+    "url": "/bacc-mada-corrige-toute-matiere/static/js/main.ff4663ff.chunk.js"
+  },
+  {
+    "revision": "0c7fe2199960ec41a454",
+    "url": "/bacc-mada-corrige-toute-matiere/static/js/runtime-main.a66140f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bacc-mada-corrige-toute-matiere/static/media/logo.5d5d9eef.svg"
+  }
+]);
